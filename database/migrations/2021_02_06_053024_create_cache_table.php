8879c0148fe58e8cacd9cb5ec7d136cd2dce2315
AquaroTorres <atorresf@gmail.com>
2022-06-06 12:24:40 -0400

    sessions_neosalud

--- database/migrations/2021_02_06_053024_create_cache_table.php
+++ database/migrations/2021_02_06_053024_create_cache_table.php
@@ -13,7 +13,7 @@ class CreateCacheTable extends Migration
      */
     public function up()
     {
-        Schema::create('cache', function (Blueprint $table) {
+        Schema::create('cache_neosalud', function (Blueprint $table) {
             $table->string('key')->unique();
             $table->mediumText('value');
             $table->integer('expiration');
@@ -27,6 +27,6 @@ class CreateCacheTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cache');
+        Schema::dropIfExists('cache_neosalud');
     }
 }
