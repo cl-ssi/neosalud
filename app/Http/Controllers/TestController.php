16129c9865d0c44178a0b8f4f85193e4e89f2ef7
AquaroTorres <atorresf@gmail.com>
2022-06-07 23:08:41 -0400

    Error log test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Log;
 
 class TestController extends Controller
 {
@@ -20,6 +21,7 @@ class TestController extends Controller
 
     public function error()
     {
+        Log::info('Boom');
         abort(500, "The Partner was not found");
     }
 }
