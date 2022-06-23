4141b8808521a8d16b1a9f81a6eeabb738161627
Jorge Miranda <mirandal.jorge@gmail.com>
2022-06-23 12:43:20 -0400

    SAMU: mobile in service create BS v5.2

--- resources/views/samu/mobileinservice/create.blade.php
+++ resources/views/samu/mobileinservice/create.blade.php
@@ -2,24 +2,24 @@
 
 @section('content')
 
-@include('samu.nav')  
+@include('samu.nav')
 
 <h3 class="mb-3"><i class="fas fa-ambulance"></i> Asigna un móvil a un turno</h3>
 
 <form method="POST" action="{{ route('samu.mobileinservice.store') }}">
     @csrf
     @method('POST')
-    
-    <div class="form-row">
 
-        <fieldset class="form-group col-8 col-md-1">
+    <div class="row">
+
+        <fieldset class="form-group col-sm-3">
             <label for="for-position">Posición salida</label>
-            <input type="number" value="1" class="form-control" name="position" id="for-position">             
+            <input type="number" value="1" class="form-control" name="position" id="for-position">
         </fieldset>
-        
-        <fieldset class="form-group col-8 col-md-2">
+
+        <fieldset class="form-group col-sm-3">
             <label for="for-mobile-id">Móvil*</label>
-            <select class="form-control" name="mobile_id" id="for-mobile-id" required>
+            <select class="form-select" name="mobile_id" id="for-mobile-id" required>
             <option></option>
                 @foreach($mobiles as $mobile)
                 @if ($mobile->managed == true)
@@ -29,9 +29,9 @@
             </select>
         </fieldset>
 
-        <fieldset class="form-group col-12 col-md-2">
+        <fieldset class="form-group col-sm-3">
             <label for="for-type">Tipo de móvil*</label>
-            <select class="form-control" name="type_id" id="for-type" required>
+            <select class="form-select" name="type_id" id="for-type" required>
                 <option value=""></option>
                 @foreach($types as $id => $name)
                     <option value="{{ $id }}">{{ $name }}</option>
@@ -39,24 +39,29 @@
             </select>
         </fieldset>
 
-        <fieldset class="form-group col-8 col-md-3">
+        <fieldset class="form-group col-sm-3">
             <label for="for-o2">Oxígeno Central</label>
-            <input type="text" class="form-control" name="o2" id="for-o2">             
+            <input type="text" class="form-control" name="o2" id="for-o2">
         </fieldset>
 
     </div>
+
+    <br>
+
     <div class="form-row">
 
-        <fieldset class="form-group col-12 col-md-8">
+        <fieldset class="form-group col-sm">
             <label for="for-observation">Observación</label>
             <textarea class="form-control" name="observation" id="for-observation"></textarea>
         </fieldset>
 
     </div>
 
-    <button type="submit" class="btn btn-primary button">Guardar</button>
-    <a href="{{ route('samu.mobileinservice.index') }}" class="btn btn-outline-secondary">Cancelar</a>
-    
+    <br>
+
+    <a href="{{ route('samu.mobileinservice.index') }}" class="btn btn-outline-secondary float-end ms-1">Cancelar</a>
+    <button type="submit" class="btn btn-primary float-end">Guardar</button>
+
 </form>
 
 @endsection
