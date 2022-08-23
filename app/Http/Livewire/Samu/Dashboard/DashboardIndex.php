189cdf8863bcdb2f066262b91cf363dc5c2c63fd
rafael1delgado <delgadorafael2011@gmail.com>
2022-08-23 17:24:02 -0400

    [refact] add samu charts with refact

--- app/Http/Livewire/Samu/Dashboard/DashboardIndex.php
+++ app/Http/Livewire/Samu/Dashboard/DashboardIndex.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Livewire\Samu\Dashboard;
 
-use App\Charts\SampleChartTwo;
 use App\Charts\Samu\EventByCommune;
 use App\Charts\Samu\EventByMobile;
 use App\Charts\Samu\EventByMonth;
@@ -12,46 +11,23 @@ use Livewire\Component;
 
 class DashboardIndex extends Component
 {
-    public $month;
+    public $event_last_month;
+    public $event_by_commune;
+    public $event_by_mobile;
+    public $event_by_month;
+    public $event_by_gender;
+
+    public function mount()
+    {
+        $this->event_last_month = (new EventLastMonth())->getDataset();
+        $this->event_by_commune = (new EventByCommune())->getDataset();
+        $this->event_by_mobile = (new EventByMobile())->getDataset();
+        $this->event_by_month = (new EventByMonth())->getDataset();
+        $this->event_by_gender = (new EventBySex())->getDataset();
+    }
 
     public function render()
     {
-        $eventLastMonth = new EventLastMonth;
-        $eventLastMonth->labels($eventLastMonth->getLabel());
-        $eventLastMonth->dataset('Eventos por día', 'bar', $eventLastMonth->getDataset())
-            ->color('#491152')
-            ->backgroundColor('#491152');
-
-        $eventByCommune = new EventByCommune;
-        $eventByCommune->labels($eventByCommune->getLabel());
-        $eventByCommune->dataset('Eventos por comuna', 'bar', $eventByCommune->getDataset())
-            ->color('#006cb7')
-            ->backgroundColor('#006cb7');
-
-        $eventByMobile = new EventByMobile;
-        $eventByMobile->labels($eventByMobile->getLabel());
-        $eventByMobile->dataset('Eventos por móviles', 'bar', $eventByMobile->getDataset())
-            ->color('#006cb7')
-            ->backgroundColor('#006cb7');
-
-        $eventByMonth = new EventByMonth;
-        $eventByMonth->labels($eventByMonth->getLabel());
-        $eventByMonth->dataset('Eventos por mes', 'bar', $eventByMonth->getDataset())
-            ->color('#006cb7')
-            ->backgroundColor('#006cb7');
-
-        $eventBySex = new EventBySex;
-        $eventBySex->labels($eventBySex->getLabel())->options([ 'scales' => [ 'yAxes' => [ 'display' => false ] ] ]);
-        $eventBySex->dataset('Eventos por sexo', 'doughnut', $eventBySex->getDataset())
-            ->color(['#006cb7', '#c90076', '#491152', '#8fce00', '#5b5b5b'])
-            ->backgroundColor(['#006cb7', '#c90076', '#491152', '#8fce00', '##5b5b5b']);
-
-        return view('livewire.samu.dashboard.dashboard-index', compact([
-            'eventLastMonth',
-            'eventByCommune',
-            'eventByMobile',
-            'eventByMonth',
-            'eventBySex'
-        ]));
+        return view('livewire.samu.dashboard.dashboard-index');
     }
 }
