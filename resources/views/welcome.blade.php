4ded938347334bdcd47274c17e526e4313623210
AquaroTorres <alvarotorres@gmail.com>
2022-09-03 10:56:43 +0000

    Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,6 +88,9 @@
 					<h4 class="my-0 font-weight-normal text-center">Ingreso al sistema</h4>
 				</div>
 				<div class="card-body text-center">
+
+                    <h6 class="mb-3">Utilizando tu Clave Única</h6>
+
 					<!-- Código para visualizar botón oficial iniciar sesión con ClaveÚnica-->
 					<a class="btn-cu btn-m btn-color-estandar m-auto" 
 						href="https://www.saludiquique.app/claveunica/redirect/neosalud"
@@ -100,11 +103,13 @@
 				
 				<hr>
 				
+                <h6 class="mb-3">O con tu cuenta del sistema</h6>
+
 				<form method="POST" action="{{ route('authenticate') }}">
 					@csrf
 					<div class="form-group row">
-						<label for="run" class="col-md-4 form-label">{{ __('RUN') }}</label>
-						<div class="col-md-8">
+						<label for="run" class="col-4 col-md-4 form-label">{{ __('RUN') }}</label>
+						<div class="col-8 col-md-8">
 							<input id="run" type="text" class="form-control @error('run') is-invalid @enderror"
 								name="run" value="{{ old('run') }}" required autocomplete="run" autofocus>
 							@error('run')
@@ -115,8 +120,8 @@
 						</div>
 					</div>
 					<div class="form-group row">
-						<label for="password" class="col-md-4 form-label">{{ __('Clave') }}</label>
-						<div class="col-md-8">
+						<label for="password" class="col-4 col-md-4 form-label">{{ __('Clave') }}</label>
+						<div class="col-8 col-md-8">
 						<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" 
 								name="password" required autocomplete="current-password">
 							@error('password')
@@ -128,7 +133,7 @@
 					</div>
 
 					<div class="form-group row">
-						<div class="col-md-4 offset-md-4">
+						<div class="col-md-6 offset-md-4">
 							<div class="form-check">
 							<input class="form-check-input" type="checkbox" name="remember" id="remember" value=1 {{ old('remember') ? 'checked' : '' }}>
 								<label class="form-check-label" for="remember">{{ __('Recuerdame') }}</label>
@@ -176,7 +181,7 @@
 						<!-- <a href="https://uni.saludtarapaca.gob.cl" class="btn btn-outline-info btn-block">UniSalud</a> -->
 					</li>
 					<li class="list-group-item">
-						<a href="https://esmeralda.saludtarapaca.org" class="btn btn-outline-info btn-block">Esmeralda</a>
+						<a href="https://esmeralda.saludtarapaca.gob.cl" class="btn btn-outline-info btn-block">Esmeralda</a>
 					</li>
 					<li class="list-group-item">
 						<a href="https://i.saludiquique.cl/claveunica?redirect=L3NpcmVteC9sb2dpbmN1" class="btn btn-outline-info btn-block">Sirmx</a>
@@ -188,27 +193,24 @@
 
         </div>
 
-        <footer class="pt-4 my-md-5 pt-md-5 border-top">
-            <div class="row">
-                <div class="col-12 col-md">
-                    <img class="mb-2" src="{{ asset('images/logo_ssi_100px.png') }}" alt="Logo Servicio de Salud Iquique">
-                </div>
-                <div class="col-6 col-md">
+		<footer class="pt-4 my-md-5 pt-md-5 border-top">
+			<div class="row">
+				<div class="col-3 col-md-3">
+					<img class="mb-2" src="{{ asset('images/logo_ssi_100px.png') }}" alt="Logo Servicio de Salud Iquique">
+				</div>
+				<div class="col-1 col-md-6">
 
-                </div>
-                <div class="col-6 col-md">
-
-                </div>
-                <div class="col-6 col-md">
-                    <h5>Desarrollado por</h5>
-                    <ul class="list-unstyled text-small">
-                        <li>Departamento TIC del SSI</li>
-                        <li><a class="text-muted" href="mailto:sistemas.ssi@redsalud.gobc.">sistemas.ssi@redsalud.gob.cl</a></li>
-                        <small class="d-block mb-3 text-muted"> 2021</small>
-                    </ul>
-                </div>
-            </div>
-        </footer>
+				</div>
+				<div class="col-7 col-md-3">
+					<h5>Desarrollado por</h5>
+					<ul class="list-unstyled text-small">
+						<li>Departamento TIC del SSI</li>
+						<li><a class="text-muted" href="mailto:sistemas.ssi@redsalud.gobc.">sistemas.ssi@redsalud.gob.cl</a></li>
+						<small class="d-block mb-3 text-muted">2021</small>
+					</ul>
+				</div>
+			</div>
+		</footer>
     </div>
 
 
