f74fc18167faebf3c0fd370cfc83490c37f874da
AquaroTorres <alvarotorres@gmail.com>
2022-09-05 23:51:20 +0000

    Remove button form home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,6 @@
     <h2>Portal interno del {{ env('APP_SS') }}</h2>
     <p>Bienvenido NeoSalud, portal de salud de la región de Tarapacá.<br> 
     Acá encontrarás la información que esté disponible para tí.</p>
-    <button class="btn btn-outline-secondary" type="button">Example button</button>
 </div>
 
 @endsection
