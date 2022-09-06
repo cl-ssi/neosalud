c0d3c807988b5b98de8b4050d49431efad28455a
AquaroTorres <alvarotorres@gmail.com>
2022-09-06 10:30:06 +0000

    CU - Producción 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="h-100 p-5 bg-light border rounded-3">
-    <h2>Portal interno del {{ env('APP_SS') }}</h2>
+    <h2>Portal del {{ env('APP_SS') }}</h2>
     <p>Bienvenido NeoSalud, portal de salud de la región de Tarapacá.<br> 
     Acá encontrarás la información que esté disponible para tí.</p>
 </div>
