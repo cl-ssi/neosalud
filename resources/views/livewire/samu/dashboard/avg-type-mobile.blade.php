da71f7b5a789893a4a2219593d69ebe8f7a8a5a8
rafael1delgado <delgadorafael2011@gmail.com>
2022-09-19 11:44:57 -0400

    [feat] add tooltips

--- resources/views/livewire/samu/dashboard/avg-type-mobile.blade.php
+++ resources/views/livewire/samu/dashboard/avg-type-mobile.blade.php
@@ -16,10 +16,26 @@
                         <td class="text-left">
                             {{ $row['start'] }} AL {{ $row['end'] }}
                         </td>
-                        <td class="text-center">{{ $row['avg_M1'] }}</td>
-                        <td class="text-center">{{ $row['avg_M2'] }}</td>
-                        <td class="text-center">{{ $row['avg_M3'] }}</td>
-                        <td class="text-center">{{ $row['avg_Hibrido'] }}</td>
+                        <td class="text-center">
+                            <span data-toggle="tooltip" data-placement="bottom" title="{{ $row['sum_M1'] . ' / ' . $row['quantity_M1'] }}">
+                                {{ $row['avg_M1'] }}
+                            </span>
+                        </td>
+                        <td class="text-center">
+                            <span data-toggle="tooltip" data-placement="bottom" title="{{ $row['sum_M2'] . ' / ' . $row['quantity_M2'] }}">
+                                {{ $row['avg_M2'] }}
+                            </span>
+                        </td>
+                        <td class="text-center">
+                            <span data-toggle="tooltip" data-placement="bottom" title="{{ $row['sum_M3'] . ' / ' . $row['quantity_M3'] }}">
+                                {{ $row['avg_M3'] }}
+                            </span>
+                        </td>
+                        <td class="text-center">
+                            <span data-toggle="tooltip" data-placement="bottom" title="{{ $row['sum_Hibrido'] . ' / ' . $row['quantity_Hibrido'] }}">
+                                {{ $row['avg_Hibrido'] }}
+                            </span>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
