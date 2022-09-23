fe96b7944b861c0486fa6c3fe6561d821981d346
rafael1delgado <delgadorafael2011@gmail.com>
2022-09-23 10:41:20 -0400

    feat: add StoreShiftRequest & UpdateShiftRequest

---
+++ app/Http/Requests/Shift/UpdateShiftRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Shift;
+
+use App\Models\Samu\Shift;
+use App\Rules\NextShift;
+use App\Rules\PreviousShift;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class UpdateShiftRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(Request $request)
+    {
+        return [
+            'type'          => 'required',
+            'opening_at'    => [
+                'required',
+                'date_format:Y-m-d\TH:i',
+                new PreviousShift($request)
+            ],
+            'closing_at'    => [
+                'nullable',
+                'date_format:Y-m-d\TH:i',
+                'after:opening_at',
+                new NextShift($request)
+            ],
+            'observation'   => 'nullable|min:0|max:5000',
+            'status'        => 'nullable'
+        ];
+    }
+}
