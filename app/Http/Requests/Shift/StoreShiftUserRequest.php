49b65c8b1cea4bdfb95e1fa0823b24d373c31b85
rafael1delgado <delgadorafael2011@gmail.com>
2022-09-27 21:02:26 -0400

    feat: add shift request

---
+++ app/Http/Requests/Shift/StoreShiftUserRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Shift;
+
+use App\Models\Samu\Shift;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreShiftUserRequest extends FormRequest
+{
+    public $shift;
+
+    public function __construct(Shift $shift)
+    {
+        $this->shift = $shift;
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'user_id'       => 'required|exists:users,id',
+            'job_type_id'   => 'required|exists:samu_job_types,id',
+            'assumes_at'    => 'required|date_format:Y-m-d\TH:i|after_or_equal:' . $this->shift->opening_at,
+            'leaves_at'     => 'nullable|date_format:Y-m-d\TH:i|after:assumes_at'
+        ];
+    }
+}
