837c22dabfaf40396f01a1c24f5c68fc8909a257
rafael1delgado <delgadorafael2011@gmail.com>
2022-10-27 20:11:04 -0400

    feat: add show fonasa data view

--- resources/views/livewire/show-fonasa-data.blade.php
+++ resources/views/livewire/show-fonasa-data.blade.php
@@ -1,59 +1,134 @@
-<div class="row g-2">
+<div>
 
-    <fieldset class="form-group col-sm-2">
-        <label for="for-identifier-type">Tipo de identificación</label>
-        <select class="form-select" id="for-identifier-type"
-            name="patient_identifier_type_id" 
-            wire:model="patient_identifier_type_id">
-            
-            <option value="">Seleccione...</option>
-            @foreach($identifierTypes as $text => $id)
-                <option value="{{ $id }}">{{ $text }}</option>
-            @endforeach
-        </select>
-    </fieldset>
+    <div class="row g-2">
 
-    @if($runInput)
-    <fieldset class="form-group col-sm-3">
-        <label for="for-run">Run</label>
-        <div class="input-group">
-            <input type="number" class="form-control" id="for-run"
-                wire:model.debounce.500ms="run"
-                placeholder="Sin dígito verificador"
-                oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
-                maxlength = "8" required>
+        <fieldset class="form-group col-sm-2">
+            <label for="for-identifier-type">Tipo de identificación</label>
+            <select
+                class="form-select"
+                id="for-identifier-type"
+                name="patient_identifier_type_id"
+                wire:model="patient_identifier_type_id"
+            >
+                <option value="">Seleccione...</option>
+                @foreach($identifierTypes as $text => $id)
+                    <option value="{{ $id }}">{{ $text }}</option>
+                @endforeach
+            </select>
+        </fieldset>
 
-            <span class="input-group-text">{{ $dv }}</span>
+        @if($runInput)
+        <fieldset class="form-group col-sm-3">
+            <label for="for-run">Run</label>
+            <div class="input-group">
+                <input
+                    type="number"
+                    class="form-control"
+                    id="for-run"
+                    wire:model.debounce.500ms="run"
+                    placeholder="Sin dígito verificador"
+                    oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);"
+                    maxlength="8"
+                    required
+                >
 
-            <button type="button" class="btn btn-success" id="for_fonasa_button" wire:click="fonasa_search">
-                <i class="fas fa-search"></i> FONASA
-            </button>
-        </div>
-        <div class="text-danger">
-            <small>{{ $error_fonasa ?? '' }}</small>
-        </div>
-    </fieldset>
-    @endif
+                <span class="input-group-text">{{ $dv }}</span>
 
-    @if($otherIdentificationInput)
-    <fieldset class="form-group col-sm-3">
-        <label for="for-patient_other_identification">Identificación</label>
-        <input type="text" class="form-control"
-            id="for-patient_other_identification"
-            wire:model.debounce.500ms="patient_other_identification">
-    </fieldset>
-    @endif
-    
-    <fieldset class="form-group col-sm-5">
-        <label for="for-patient_name">Nombre del paciente</label>
-        <input type="text" class="form-control"
-            wire:model.debounce.500ms="patient_name"
-            name="patient_name"
-            id="for-patient_name">
-    </fieldset>
-    
-    <input type="hidden" name="patient_identification" wire:model="patient_identification">
+                <button
+                    type="button"
+                    class="btn btn-success"
+                    id="for_fonasa_button"
+                    wire:click="fonasa_search"
+                >
+                    <i class="fas fa-search"></i> FONASA
+                </button>
+            </div>
+            <div class="text-danger">
+                <small>{{ $error_fonasa ?? '' }}</small>
+            </div>
+        </fieldset>
+        @endif
+
+        @if($otherIdentificationInput)
+        <fieldset class="form-group col-sm-3">
+            <label for="for-patient_other_identification">Identificación</label>
+            <input
+                type="text"
+                class="form-control"
+                id="for-patient_other_identification"
+                wire:model.debounce.500ms="patient_other_identification"
+            >
+        </fieldset>
+        @endif
+
+        <fieldset class="form-group col-sm-5">
+            <label for="for-patient_name">Nombre del paciente</label>
+            <input
+                type="text"
+                class="form-control"
+                wire:model.debounce.500ms="patient_name"
+                name="patient_name"
+                id="for-patient_name"
+                @if($disabled) readonly @endif
+            >
+        </fieldset>
+
+        <input type="hidden" name="patient_identification" wire:model="patient_identification">
+    </div>
+
+    <div class="row g-2 mt-2">
+        <input type="hidden" name="prevision" value="{{ $prevision }}">
+        <input type="hidden" name="gender_id" value="{{ $gender_id }}">
+        <input type="hidden" name="verified_fonasa_at" value="{{ $verified_fonasa_at }}">
+        <input type="hidden" name="run_fixed" value="{{ $run_fixed }}">
+
+        <fieldset class="form-group col-sm-3">
+            <label for="for-gender-id">Género</label>
+            <select
+                class="form-select input-disabled"
+                id="for-gender-id"
+                name="gender_id"
+                wire:model="gender_id"
+                @if($disabled) disabled @endif
+            >
+                <option value="">Seleccione un genero</option>
+                @foreach($genders as $gender)
+                    <option value="{{ $gender->id }}">{{ $gender->text }}</option>
+                @endforeach
+            </select>
+        </fieldset>
+
+        <fieldset class="form-group col-sm-3">
+            <label for="for-prevision">Previsión</label>
+            <select
+                class="form-select input-disabled"
+                id="for-prevision"
+                name="prevision"
+                wire:model="prevision"
+                @if($disabled) disabled @endif
+            >
+                <option value="">Seleccione una previsión</option>
+                <option value="FONASA A">FONASA A</option>
+                <option value="FONASA B">FONASA B</option>
+                <option value="FONASA C">FONASA C</option>
+                <option value="FONASA D">FONASA D</option>
+                <option value="FONASA D">FONASA E</option>
+                <option value="ISAPRE">ISABPRE</option>
+
+            </select>
+        </fieldset>
+
+        <fieldset class="form-group col-sm-3">
+            <label for="for-birthday">Fecha Nacimiento</label>
+            <input
+                type="date"
+                class="form-control"
+                id="for-birthday"
+                name="birthday"
+                wire:model.debounce.500ms="birthday"
+                @if($disabled) readonly @endif
+            >
+        </fieldset>
+    </div>
 
 </div>
-
-
