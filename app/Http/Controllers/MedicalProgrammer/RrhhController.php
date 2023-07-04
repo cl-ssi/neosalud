45521473624d6438993ceeb3765b9203d1188e7e
sick <esteban.rojasg@redsalud.cl>
2023-07-04 10:05:24 -0400

    fix carga archivo sirh

--- app/Http/Controllers/MedicalProgrammer/RrhhController.php
+++ app/Http/Controllers/MedicalProgrammer/RrhhController.php
@@ -645,126 +645,130 @@ class RrhhController extends Controller
                         //se debe modificar esto, puede haber más de un contrato en el año,
                         //si existe un contrato para una persona en el mismo periodo (inicio a termino), se modifica. ¿?
                         //si no, se crea uno nuevo.
-                        $contract = Contract::where('user_id',$user->id)
-                                            // ->where('year',$fecha_inicio_contrato_ddmmaaaa->format('Y'))
-                                            ->where('contract_start_date',$fecha_inicio_contrato_ddmmaaaa)
-                                            ->where('contract_end_date',$fecha_termino_contrato_ddmmaaaa)
-                                            ->where('contract_id',$column['correlativo_contrato'])
-                                            ->get();
+                        
+                        // $contract = Contract::where('user_id',$user->id)
+                        //                     // ->where('year',$fecha_inicio_contrato_ddmmaaaa->format('Y'))
+                        //                     ->where('contract_start_date',$fecha_inicio_contrato_ddmmaaaa)
+                        //                     ->where('contract_end_date',$fecha_termino_contrato_ddmmaaaa)
+                        //                     ->where('contract_id',$column['correlativo_contrato'])
+                        //                     ->get();
 
                         $establishment_id = Organization::where('code_deis',$column['id_deis'])->first()->id;
 
-                        //si no encuentra contrato, lo crea.
-                        if($contract->count() == 0){
-                            $contract = new Contract();
-                            $contract->user_id = $user->id;
-                            $contract->establishment_id = $establishment_id;
-                            $contract->year = $fecha_inicio_contrato_ddmmaaaa->format('Y');
-                            switch ($column['ley']) {
-                                case 19664:
-                                    $contract->law = 'LEY 19.664';
-                                    break;
-                                case 18834:
-                                    $contract->law = 'LEY 18.834';
-                                    break;
-                                case 15076:
-                                    $contract->law = 'LEY 15.076';
-                                    break;
-                                case 'Honorarios':
-                                    $contract->law = 'HSA';
-                            }
-                            $contract->contract_id = $column['correlativo_contrato'];
-                            switch ($column['sistema_de_turno_sino']) {
-                                case 'No':
-                                    $contract->shift_system = 'N';
-                                    break;
-                                case 'Si':
-                                    $contract->shift_system = 'S';
-                                    break;
-                            }
+                        // //si no encuentra contrato, lo crea.
+                        // if($contract->count() == 0){
+                        //     $contract = new Contract();
+                        //     $contract->user_id = $user->id;
+                        //     $contract->establishment_id = $establishment_id;
+                        //     $contract->year = $fecha_inicio_contrato_ddmmaaaa->format('Y');
+                        //     switch ($column['ley']) {
+                        //         case 19664:
+                        //             $contract->law = 'LEY 19.664';
+                        //             break;
+                        //         case 18834:
+                        //             $contract->law = 'LEY 18.834';
+                        //             break;
+                        //         case 15076:
+                        //             $contract->law = 'LEY 15.076';
+                        //             break;
+                        //         case 'Honorarios':
+                        //             $contract->law = 'HSA';
+                        //     }
+                        //     $contract->contract_id = $column['correlativo_contrato'];
+                        //     switch ($column['sistema_de_turno_sino']) {
+                        //         case 'No':
+                        //             $contract->shift_system = 'N';
+                        //             break;
+                        //         case 'Si':
+                        //             $contract->shift_system = 'S';
+                        //             break;
+                        //     }
+                        //     // $contract->shift_system = $column['sistema_de_turno_sino'];
+                        //     $contract->weekly_hours = $column['hrs_semanales_contratadas'];
+                        //     $contract->effective_hours = $column['horas_efectivas_al_centro'];
+                        //     $contract->weekly_collation = $column['tiempo_de_colacion_semanal_min'];
+                        //     $contract->weekly_union_permit = $column['tiempo_de_permiso_gremial_semanal_min'];
+                        //     $contract->breastfeeding_time = $column['tiempo_de_lactancia_semanal_min'];
+                        //     $contract->obs = $column['observaciones_debe_identificar_liberado_de_guardia_lgperiodo_asistencial_obligatoriopaobecario_beca'];
+                        //     $contract->legal_holidays = $column['feriados_legales'][0];
+                        //     $contract->compensatory_rest = $column['dias_descanso_compensatorio_ley_urgencia'][0];
+                        //     $contract->administrative_permit = $column['dias_de_permisos_administrativos'][0];
+                        //     $contract->covid_permit = $column['descanso_reparatorio_covid'][0];
+                        //     $contract->training_days = $column['dias_de_congreso_o_capacitacion'][0];
+                        //     // $contract->covid_permit = $column['rut_programable'];
+                        //     $contract->contract_start_date = $fecha_inicio_contrato_ddmmaaaa;
+                        //     $contract->contract_end_date = $fecha_termino_contrato_ddmmaaaa;
+                        //     $contract->departure_date = $fecha_alejamiento_ddmmaaaa;
+                        //     $contract->service_id = 50; //sin servicio
+                        //     $contract->save();
+
+                        //     $contract_count = $contract_count + 1;
+                        // }
+
+                        switch ($column['ley']) {
+                            case 19664:
+                                $contract_law = 'LEY 19.664';
+                                break;
+                            case 18834:
+                                $contract_law = 'LEY 18.834';
+                                break;
+                            case 15076:
+                                $contract_law = 'LEY 15.076';
+                                break;
+                            case 'Honorarios':
+                                $contract_law = 'HSA';
+                        };
+
+                        switch ($column['sistema_de_turno_sino']) {
+                            case 'No':
+                                $contract_shift_system = 'N';
+                                break;
+                            case 'Si':
+                                $contract_shift_system = 'S';
+                                break;
+                        };
+
+                        $contract = Contract::updateOrCreate(
+                        [
+                            'user_id' => $user->id,
+                            'contract_start_date' => $fecha_inicio_contrato_ddmmaaaa,
+                            'contract_end_date'=> $fecha_termino_contrato_ddmmaaaa,
+                            'contract_id' => $column['correlativo_contrato']
+                        ],
+                        [
+                            'user_id' => $user->id,
+                            'establishment_id' => $establishment_id,
+                            'year' => $fecha_inicio_contrato_ddmmaaaa->format('Y'),
+                            'law' => $contract_law,
+                            'contract_id' => $column['correlativo_contrato'],
+                            'shift_system' => $contract_shift_system,
                             // $contract->shift_system = $column['sistema_de_turno_sino'];
-                            $contract->weekly_hours = $column['hrs_semanales_contratadas'];
-                            $contract->effective_hours = $column['horas_efectivas_al_centro'];
-                            $contract->weekly_collation = $column['tiempo_de_colacion_semanal_min'];
-                            $contract->weekly_union_permit = $column['tiempo_de_permiso_gremial_semanal_min'];
-                            $contract->breastfeeding_time = $column['tiempo_de_lactancia_semanal_min'];
-                            $contract->obs = $column['observaciones_debe_identificar_liberado_de_guardia_lgperiodo_asistencial_obligatoriopaobecario_beca'];
-                            $contract->legal_holidays = $column['feriados_legales'][0];
-                            $contract->compensatory_rest = $column['dias_descanso_compensatorio_ley_urgencia'][0];
-                            $contract->administrative_permit = $column['dias_de_permisos_administrativos'][0];
-                            $contract->covid_permit = $column['descanso_reparatorio_covid'][0];
-                            $contract->training_days = $column['dias_de_congreso_o_capacitacion'][0];
+                            'weekly_hours' => $column['hrs_semanales_contratadas'],
+                            'effective_hours' => $column['horas_efectivas_al_centro'],
+                            'weekly_collation' => $column['tiempo_de_colacion_semanal_min'],
+                            'weekly_union_permit' => $column['tiempo_de_permiso_gremial_semanal_min'],
+                            'breastfeeding_time' => $column['tiempo_de_lactancia_semanal_min'],
+                            'obs' => $column['observaciones_debe_identificar_liberado_de_guardia_lgperiodo_asistencial_obligatoriopaobecario_beca'],
+                            'legal_holidays' => $column['feriados_legales'][0],
+                            'compensatory_rest' => $column['dias_descanso_compensatorio_ley_urgencia'][0],
+                            'administrative_permit' => $column['dias_de_permisos_administrativos'][0],
+                            'covid_permit' => $column['descanso_reparatorio_covid'][0],
+                            'training_days' => $column['dias_de_congreso_o_capacitacion'][0],
                             // $contract->covid_permit = $column['rut_programable'];
-                            $contract->contract_start_date = $fecha_inicio_contrato_ddmmaaaa;
-                            $contract->contract_end_date = $fecha_termino_contrato_ddmmaaaa;
-                            $contract->departure_date = $fecha_alejamiento_ddmmaaaa;
-                            $contract->service_id = 50; //sin servicio
-                            $contract->save();
+                            'contract_start_date' => $fecha_inicio_contrato_ddmmaaaa,
+                            'contract_end_date' => $fecha_termino_contrato_ddmmaaaa,
+                            'departure_date' => $fecha_alejamiento_ddmmaaaa,
+                            'service_id' => 50 //sin servicio
+                        ]);
 
-                            $contract_count = $contract_count + 1;
-                        }
-                        else{
-                            // se modifica con info de excel
-
-                            $contract = Contract::where('user_id',$user->id)
-                                                ->where('contract_start_date',$fecha_inicio_contrato_ddmmaaaa)
-                                                ->where('contract_end_date',$fecha_termino_contrato_ddmmaaaa)
-                                                ->where('contract_id',$column['correlativo_contrato'])
-                                                ->first();
-
-                            // $contract->user_id = $user->id;
-                            $contract->establishment_id = $establishment_id;
-                            $contract->year = $fecha_inicio_contrato_ddmmaaaa->format('Y');
-                            switch ($column['ley']) {
-                                case 19664:
-                                    $contract->law = 'LEY 19.664';
-                                    break;
-                                case 18834:
-                                    $contract->law = 'LEY 18.834';
-                                    break;
-                                case 15076:
-                                    $contract->law = 'LEY 15.076';
-                                    break;
-                                case 'Honorarios':
-                                    $contract->law = 'HSA';
-                            }
-                            $contract->contract_id = $column['correlativo_contrato'];
-                            switch ($column['sistema_de_turno_sino']) {
-                                case 'No':
-                                    $contract->shift_system = 'N';
-                                    break;
-                                case 'Si':
-                                    $contract->shift_system = 'S';
-                                    break;
-                            }
-                            // $contract->shift_system = $column['sistema_de_turno_sino'];
-                            $contract->weekly_hours = $column['hrs_semanales_contratadas'];
-                            $contract->effective_hours = $column['horas_efectivas_al_centro'];
-                            $contract->weekly_collation = $column['tiempo_de_colacion_semanal_min'];
-                            $contract->weekly_union_permit = $column['tiempo_de_permiso_gremial_semanal_min'];
-                            $contract->breastfeeding_time = $column['tiempo_de_lactancia_semanal_min'];
-                            $contract->obs = $column['observaciones_debe_identificar_liberado_de_guardia_lgperiodo_asistencial_obligatoriopaobecario_beca'];
-                            $contract->legal_holidays = $column['feriados_legales'][0];
-                            $contract->compensatory_rest = $column['dias_descanso_compensatorio_ley_urgencia'][0];
-                            $contract->administrative_permit = $column['dias_de_permisos_administrativos'][0];
-                            $contract->covid_permit = $column['descanso_reparatorio_covid'][0];
-                            $contract->training_days = $column['dias_de_congreso_o_capacitacion'][0];
-                            // $contract->covid_permit = $column['rut_programable'];
-                            $contract->contract_start_date = $fecha_inicio_contrato_ddmmaaaa;
-                            $contract->contract_end_date = $fecha_termino_contrato_ddmmaaaa;
-                            $contract->departure_date = $fecha_alejamiento_ddmmaaaa;
-                            $contract->service_id = 50; //sin servicio
-                            $contract->save();
-
-                            $contract_count = $contract_count + 1;
-                        }
+                        $contract_count = $contract_count + 1;
                     }
                 }
             }
             
         }
 
-        session()->flash('success', 'Se ha cargado correctamente el archivo (Se han creado ' . $user_count . ' usuarios y ' . $contract_count . ' contratos).');
+        session()->flash('success', 'Se ha cargado correctamente el archivo (Se han creado/modificado ' . $user_count . ' usuarios y ' . $contract_count . ' contratos).');
         return redirect()->back();
     }
 
