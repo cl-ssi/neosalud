cb408fc40d10eafd59e01a44756408bcfebe3936
rafael1delgado <delgadorafael2011@gmail.com>
2023-10-24 10:54:10 -0400

    feat: adds graph and component of calls by classification

---
+++ app/Http/Livewire/Samu/Dashboard/CallsByClassification.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Livewire\Samu\Dashboard;
+
+use App\Charts\Samu\CallsByClassification as SamuCallsByClassification;
+use Livewire\Component;
+
+class CallsByClassification extends Component
+{
+    public $year;
+    public $month;
+    public $year_month;
+    public $chartData;
+
+    public function mount()
+    {
+        $this->year = $this->year ? $this->year : now()->year;
+        $this->month = $this->month ? $this->month : now()->month;
+        $this->year_month = $this->year . "-" . $this->month;
+    }
+
+    public function render()
+    {
+        return view('livewire.samu.dashboard.calls-by-classification');
+    }
+
+    public function getData()
+    {
+        $options = [
+            'Clasificación',
+            'Total de llamadas',
+            ["role" => 'style'],
+            ["role" => 'annotation'],
+        ];
+
+        $chartData = (new SamuCallsByClassification($this->year, $this->month))->getDataset();
+
+        array_unshift($chartData, $options);
+
+        $this->emit('calls-by-classification', $chartData);
+    }
+
+    public function updatedYearMonth($yearMonth)
+    {
+        $yearMonth = explode("-", $yearMonth);
+        $this->year = $yearMonth[0];
+        $this->month = $yearMonth[1];
+        $this->getData();
+    }
+}
