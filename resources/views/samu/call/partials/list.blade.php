98005420ae4cd518568be15d094903d6a9dc8ad0
Ignacio Miranda <piranga8@gmail.com>
2025-08-09 12:53:22 -0400

    SAMU: fix fecha llamadas.

--- resources/views/samu/call/partials/list.blade.php
+++ resources/views/samu/call/partials/list.blade.php
@@ -49,7 +49,7 @@
                     </a>
                     @endif
                 </td>
-                <td width="90">{{ $call->hour->format('d-m-Y h:s') }}</td>
+                <td width="90">{{ $call->hour->format('d-m-Y H:i:s') }}</td>
                 <td>{{ $call->applicant }}</td>
                 <td>
                     {{ $call->sex_abbr }}
